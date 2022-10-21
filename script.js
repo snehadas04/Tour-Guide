c794f3c16fc1b28b7b1802e96650d1c4832571b9
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2022-10-21 22:32:40 +0530

    Day and Night mode active

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
-let navbar = document.querySelector('.header .navbar');
+let navbar = document.querySelector('.navbar')
 
 document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.add('active');
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
 }
 
-document.querySelector('#nav-close').onclick = () =>{
+let loginForm = document.querySelector('.login-form')
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
     navbar.classList.remove('active');
+    searchForm.classList.remove('active');
 }
 
-let searchForm = document.querySelector('.search-form');
+let searchForm = document.querySelector('.search-form')
 
 document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.add('active');
-}
-
-document.querySelector('#close-search').onclick = () =>{
-    searchForm.classList.remove('active');
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
 }
 
 window.onscroll = () =>{
     navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}
 
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
+let themeBtn = document.querySelector('#theme-btn');
+
+themeBtn.onclick = () =>{
+    themeBtn.classList.toggle('fa-sun');
+
+    if(themeBtn.classList.contains('fa-sun')){
+        document.body.classList.add('active');
     }else{
-        document.querySelector('.header').classList.remove('active');
+        document.body.classList.remove('active');
     }
-};
 
-window.onload = () =>{
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.remove('active');
-    }
 };
-
 
 var swiper = new Swiper(".home-slider", {
     loop:true, 
@@ -46,88 +50,15 @@ var swiper = new Swiper(".home-slider", {
     },
 });
 
-var swiper = new Swiper(".product-slider", {
-    loop:true, 
-    grabCursor:true,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        640: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        1024: {
-          slidesPerView: 4,
-        },
-    },
-});
-
 var swiper = new Swiper(".review-slider", {
-    loop:true, 
-    grabCursor:true,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    loop:true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
     },
-    breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        640: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-    },
-});
-
-var swiper = new Swiper(".blogs-slider", {
-    loop:true, 
-    grabCursor:true,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        991: {
-          slidesPerView: 3,
-        },
-    },
-});
-
-var swiper = new Swiper(".clients-slider", {
-    loop:true, 
-    grabCursor:true,
-    spaceBetween: 20,
-    breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        640: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        1024: {
-          slidesPerView: 4,
-        },
+    pagination: {
+        el: ".swiper-pagination",
     },
 });
